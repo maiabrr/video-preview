83cd4af518e0633a177321371fa998a3e253210b
maiabrr <maiabelenr@gmail.com>
2024-04-23 18:44:03 -0400

    Video Hover

---
+++ script.js
@@ -0,0 +1,9 @@
+function playVideo() {
+    const mainVideo = document.getElementById('mainVideo');
+    mainVideo.play();
+}
+
+function pauseVideo() {
+    const mainVideo = document.getElementById('mainVideo');
+    mainVideo.pause();
+}
